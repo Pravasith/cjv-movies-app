60aa0454001256b8fb15ecfa140b03be5bc0c7ea
pravasith <pravasith@gmail.com>
2022-07-06 14:42:33 -0400

    added slider

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -2,50 +2,75 @@ import Image from "../UI/Image"
 
 import leftArrow from "../../assets/images/left.webp"
 import Icon from "../UI/Icon"
-import { useState } from "react"
-
-const image = {
-    src: "https://i.pinimg.com/736x/c3/e4/24/c3e424e35083c15a4bd106c46fd051f5.jpg",
-}
+import { useEffect, useState } from "react"
+import { movieBackdrops } from "../data"
 
 const HeroSection = props => {
     const [slideCount, setSlideCount] = useState(0)
 
     const clickHandler = dir => {
         if (dir === "r") setSlideCount(c => c - 1)
-        else setSlideCount(c => c + 1)
+        else if (dir === "l") setSlideCount(c => c + 1)
     }
+
+    useEffect(() => {
+        console.log(slideCount)
+    }, [slideCount])
 
     const SLIDE_PERCENTAGE = 100
 
     return (
         <div className="relative w-full ">
-            <div className="w-full overflow-hidden">
+            <div className="w-full overflow-hidden flex-row">
                 <div
                     style={{
                         transform: `translate(${
                             slideCount * SLIDE_PERCENTAGE
                         }%)`,
                     }}
-                    className={`transition ease-in-out delay-150`}
+                    className={` transition ease-in-out delay-150 flex-row-west w-full`}
                 >
-                    <Image image={image} />
+                    {movieBackdrops.map((movie, i) => {
+                        const movieMorph = {
+                            src: movie.backdrop,
+                        }
+
+                        return (
+                            <div
+                                className="min-w-full"
+                                key={"movie-backdrop-" + i}
+                            >
+                                {<Image image={movieMorph} />}
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
 
             <div className="absolute left-4 top-0 h-full">
                 <div className="flex-col-center h-full">
-                    <Icon src={leftArrow} onClick={() => clickHandler("l")} />
+                    {slideCount < 0 && (
+                        <Icon
+                            src={leftArrow}
+                            onClick={() => {
+                                clickHandler("l")
+                            }}
+                        />
+                    )}
                 </div>
             </div>
 
             <div className="absolute right-4 top-0 h-full">
                 <div className="flex-col-center h-full">
-                    <Icon
-                        src={leftArrow}
-                        rotate
-                        onClick={() => clickHandler("r")}
-                    />
+                    {-slideCount < movieBackdrops.length - 1 && (
+                        <Icon
+                            src={leftArrow}
+                            rotate
+                            onClick={() => {
+                                clickHandler("r")
+                            }}
+                        />
+                    )}
                 </div>
             </div>
         </div>
--- src/components/UI/Image/index.jsx
+++ src/components/UI/Image/index.jsx
@@ -2,8 +2,8 @@ const Image = props => {
     const { image } = props
 
     return (
-        <div className="flex-col-center  rounded-xl bg-black h-80 my-6 mx-2">
-            <img className="object-contain h-80 p-0" src={image.src} />
+        <div className="flex-col-center  rounded-xl bg-black h-96 my-6 mx-2">
+            <img className="object-contain h-96 p-0" src={image.src} />
         </div>
     )
 }
